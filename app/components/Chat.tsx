c52ea6c9a82b672ff524e8646cbe72a58f39da2b
Andy Choi <andychoi@Andys-MacBook-Pro-5.local>
2023-10-11 12:44:46 -0400

    Improve UI and Fix Bugs

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -1,19 +1,29 @@
-import { useState, useEffect } from 'react';
+// Importing necessary hooks and components from React and other libraries
+import { useState, useEffect, useCallback } from 'react';
 import { useChat } from 'ai/react'; 
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 
-const Chat = ({ initialInput }) => {
+// Defining TypeScript interface for Chat component properties
+interface ChatProps {
+  initialInput: string;
+}
+
+// Defining the Chat functional component that takes initialInput as a prop
+const Chat: React.FC<ChatProps>  = ({ initialInput }) => {
+  // State variables initialization for controlling the chat UI and behavior
   const [submitType, setSubmitType] = useState<'text'|'image'>("text");
   const [imageUrl, setImageUrl] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  const { messages, input, handleInputChange, handleSubmit: handleChatSubmit } = useChat({
+  // Using a custom hook useChat which probably provides messaging functionalities
+  const { append, messages, input, handleInputChange, handleSubmit: handleChatSubmit } = useChat({
     api: '/api/openai',
   });
 
-  const getImageData = async () => {
+  // Function to fetch an image from the DALL-E API based on the chat input
+   const getImageData = async () => {
     try {
       const response = await fetch('/api/dall-e', {
         method: 'POST',
@@ -29,9 +39,9 @@ const Chat = ({ initialInput }) => {
       setError(`An error occurred calling the API: ${e}`);
     }
     setLoading(false);
-  };
+  }; 
 
-  const handleSubmit = (event?: React.FormEvent<HTMLFormElement>) => {
+  /*const handleSubmit = (event?: React.FormEvent<HTMLFormElement>) => {
     if (event) {
       event.preventDefault();
       handleChatSubmit(event);
@@ -49,41 +59,78 @@ const Chat = ({ initialInput }) => {
       handleInputChange({ target: { value: initialInput } });
       handleSubmit();
     }
-  }, [initialInput]);
+  }, [initialInput, handleInputChange, handleSubmit]); */
 
+  // Using the useCallback hook to create a memoized function to update the input value
+  const updateInputValue = useCallback((value: string) => {
+    handleInputChange({ target: { value } } as React.ChangeEvent<HTMLInputElement>);
+}, [handleInputChange]);
+
+  
+  // useEffect hook to append the initialInput to the chat if provided
+  useEffect(() => {
+    if (initialInput) {
+      append({
+        content: initialInput,
+        role: 'user'
+      });
+    }
+  },[ initialInput]);
+
+  /*const userColors = {
+    user: '#fff',
+    assistant: '#fff',
+  }; */
+
+  // Updated object for user colors
   const userColors = {
     user: '#fff',
     assistant: '#fff',
-  };
+    function: '#fff',
+    system: '#fff',
+};
 
+
+  // This function is used to display chat messages or show the generated image based on the submit type
   const renderResponse = () => {
+    // Check if the submit type is 'text' which means the response to render should be text-based chat messages.
     if (submitType === 'text') {
       return (
+        // Container for the response
         <div className="response">
           {messages.length > 0
+          // If there are messages, loop through each message and display them
             ? messages.map(m => (
                 <div key={m.id} className="chat-line">
+                  {/* Display the role of the sender (user or Senior-SoftwareEngineer). The role's text color is determined by the userColors object based on the role */}
                   <span style={{color: userColors[m.role]}}>{m.role === 'user' ? 'User: ' : '⚡️Senior-SoftwareEngineer: '}</span>
+                  {/* Convert markdown content of the message to displayable HTML */}
                   <ReactMarkdown>{m.content}</ReactMarkdown>
                 </div>
               ))
+              // If there are no messages, display the error message.
             : error}
         </div>
       );
     } else {
+      // If the submit type isn't 'text', then display an image-based response
       return (
         <div className="response">
+          {/* Show a loading spinner while the image is being fetched/processed */}
           {loading && <div className="loading-spinner"></div>}
+          {/* If there's an image URL available, display the image */}
           {imageUrl && <Image src={imageUrl} className="image-box" alt="Generated image" width="400" height="400" />}
         </div>
       );
     }
   };
 
+  // JSX code block for rendering the chat interface
   return (
+    // Fragment shorthand (<></>) is used to group multiple JSX elements without adding an extra noe to the DOM
     <>
       {renderResponse()}
-      <form onSubmit={handleSubmit} className="mainForm">
+      <form onSubmit={handleChatSubmit} className="mainForm">
         <input name="input-field" placeholder="Say anything" onChange={handleInputChange} value={input} />
         <button type="submit" className="mainButton" disabled={loading}>
           TEXT
@@ -93,4 +140,5 @@ const Chat = ({ initialInput }) => {
   );
 }
 
+// Exporting the Chat component to be used elsewhere
 export default Chat;
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,68 @@
+// Indicates that the code is for the client side
 'use client';
 
-import { useState } from 'react';
-import Image from 'next/image';
-import Chat from './components/Chat';
+// Importing necessary dependencies and components
+import { useState } from 'react'; // useState hook from React for state management
+import Image from 'next/image'; // Image component from Next.js for optimized image rendering
+import Chat from './components/Chat'; // Chat component to start the chat functionality
 
+// Main component for the home page
 export default function Home() {
-  const [position, setPosition] = useState("");
-  const [company, setCompany] = useState("");
-  const [input, setInput] = useState("");
-  const [chatStarted, setChatStarted] = useState(false);
+
+  // State variables to store the position, company, input for chat, and chat start status
+  const [position, setPosition] = useState(""); // For storing the position entered by the user
+  const [company, setCompany] = useState(""); // For storing the company name entered by the user
+  const [input, setInput] = useState(""); // For storing the combined input for the chat
+  const [chatStarted, setChatStarted] = useState(false); // To keep track if the chat has started or not
   
-  const handleChatSubmit = (event) => {
-    event.preventDefault();
+  // Handler function for the chat form submission
+  const handleChatSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Preventing default form submission behavior
+    // Check if both position and company are entered by user
     if (position && company) {
-      const combinedInput = `${position} at ${company}`; // Use backticks for template literals
-      setInput(combinedInput);
-      setChatStarted(true);
+      // Combining the position and company for chat input
+      const combinedInput = `${position} at ${company}`;
+      setInput(combinedInput); // Setting the combined input for the chat
+      setChatStarted(true); // Indicate that chat has started
     } else {
+      // If either position or company is missing, alert the user
       alert("Please enter both position and company before starting the chat.");
     }
   };
   
 
   return (
+    // Root element with class "App"
     <main className="App">
-      {/* Logo and title */}
+      {/* Section for logo and title */}
       <div className="logoTitleBox">
+        {/* Using the Next.js Image component to efficiently load and display an image with specified dimension */}
         <Image className="logo" src="/technology.png" alt="logo" width="220" height="160" />
+        {/* Displaying the title of the app */}
         <h1 className="title">AI-InterviewTech</h1>
+        {/* A brief description about the app's purpose */}
         <p>This is an interface to talk to the senior software engineer for interview preparation!</p>
       </div>
-  
+      
+      {/* Using conditional rendering based on the state of chatStarted */}
       {!chatStarted ? (
-        <>
-          {/* Position and Company Inputs */}
+        // If chat hasn't started, this block will be rendered
+        <form onSubmit={handleChatSubmit} className="mainForm">
+          {/* Input fields for capturing user's desired position and company */}
           <div className="inputsBox">
+            {/* Input for entering the desired position with a change event handler */}
             <input className="position" placeholder="Enter position" onChange={(e) => setPosition(e.target.value)} value={position} />
+            {/* Input for entering the desired company with a change event handler */}
             <input className="company" placeholder="Enter company" onChange={(e) => setCompany(e.target.value)} value={company} />
           </div>
   
-          {/* Start Chat Button */}
+          {/* Button that user clicks to start the chat */}
           <div className="startChatBox">
-            <button type="submit" onClick={handleChatSubmit}>Start Chat</button>
+            <button type="submit">Start Chat</button>
           </div>
-        </>
+        </form>
       ) : (
+        // If chat has been started, the Chat component is rendered
         <Chat initialInput={input} />
       )}
     </main>
