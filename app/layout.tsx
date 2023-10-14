f73dc095c0adb67b8e80fcec9b68621dabd40436
Andy Choi <andychoi@andys-mbp-5.mynetworksettings.com>
2023-10-14 01:50:22 -0400

    Page Layout Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Codebender AI Template',
-  description: 'Template code for the Codebender AI Project',
+  title: 'InterviewGPT',
 }
 
 export default function RootLayout({
